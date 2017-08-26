ce0224f5c36d1f9b2928f19edd60f403f2fc70ad
Geoffrey <geoffrey@geoffreys-mbp.attlocal.net>
2017-08-25 17:07:27 -0700

    CODE for email password and go button for the alignment and width and height of the fields and button

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -10,14 +10,41 @@ import UIKit
 
 class SignInVC : UIViewController {
 
+        @IBOutlet weak var emailField: UITextField!
+        @IBOutlet weak var passwordField: UITextField!
+        @IBOutlet weak var loginButton: UIButton!
+ 
+        
         override func viewDidLoad() {
                 super.viewDidLoad()
-                // Do any additional setup after loading the view, typically from a nib.
-        }
 
-        override func didReceiveMemoryWarning() {
-                super.didReceiveMemoryWarning()
-                // Dispose of any resources that can be recreated.
+                // Width
+                let screenWidth = UIScreen.main.bounds.width
+                let fieldAndButtonWidth = screenWidth * 0.7
+                emailField.frame.size.width = fieldAndButtonWidth
+                passwordField.frame.size.width = fieldAndButtonWidth
+                loginButton.frame.size.width = fieldAndButtonWidth
+                
+                // Height
+                let screenHeight = UIScreen.main.bounds.height
+                let fieldAndButtonHeight = screenHeight * 0.0598
+                emailField.frame.size.height = fieldAndButtonHeight
+                passwordField.frame.size.height = fieldAndButtonHeight
+                loginButton.frame.size.height = fieldAndButtonHeight
+                
+                // Origin Y
+                let originY = screenHeight * 0.5757
+                let inputSpace = (screenHeight * 0.0475) + emailField.frame.height
+                emailField.frame.origin.y = originY
+                passwordField.frame.origin.y = originY + inputSpace
+                loginButton.frame.origin.y = originY + (inputSpace * 2)
+                
+                // Origin X
+                let originX = (screenWidth / 2) - (emailField.frame.width / 2)
+                emailField.frame.origin.x = originX
+                passwordField.frame.origin.x = originX
+                loginButton.frame.origin.x = originX
+                
         }
 
 
